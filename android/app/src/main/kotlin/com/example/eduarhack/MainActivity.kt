1bd6570dcfa177e398231b36f02bcd9d95af0938
Rishav Raj Jain <rishavrajjain@gmail.com>
2021-01-31 21:07:39 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/eduarhack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eduarhack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = '78bd79e2962b48a4a0c100b426527f96';
+//const Token = Your_Token;
